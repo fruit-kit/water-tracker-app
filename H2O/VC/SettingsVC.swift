a4b139d510d3b15377dcbbf4b2a1e46d4f7e547a
Robert Kotrutsa <tr3asur3island@gmail.com>
2026-03-14 08:05:38 +0100

    feat: add SettingsVC

---
+++ H2O/VC/SettingsVC.swift
@@ -0,0 +1,29 @@
+//
+//  SettingsVC.swift
+//  H2O
+//
+//  Created by Robert Kotrutsa on 14.03.26.
+//
+
+import UIKit
+
+class SettingsVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
