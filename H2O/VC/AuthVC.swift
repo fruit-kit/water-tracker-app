3384b8c834088dd1ebf3dd8296bff43bbe4b9c3c
Robert Kotrutsa <tr3asur3island@gmail.com>
2026-04-20 09:17:20 +0200

    feat: Add AuthVC

---
+++ H2O/VC/AuthVC.swift
@@ -0,0 +1,17 @@
+//
+//  AuthVC.swift
+//  H2O
+//
+//  Created by Robert Kotrutsa on 20.04.26.
+//
+
+import UIKit
+
+class AuthVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+}
